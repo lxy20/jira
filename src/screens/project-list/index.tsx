7a43e180df6658b86447dc8e4af7d4ce5033baca
lixiaoyi <742805538@qq.com>
2023-09-18 10:34:42 +0800

    feat: json-server-middleware

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useEffect, useState } from "react";
+import { cleanObject, useDebounce, useMount } from "utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [list, setList] = useState([]);
+
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const [users, setUsers] = useState([]);
+
+  const debouncedParam = useDebounce(param, 500);
+
+  //获取负责人列表Users
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  //根据输入的参数获取结果列表List
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`,
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  // useEffect(() =>{
+  //     fetch(`${apiUrl}/projects?name=${param.name}&personId=${param.personId}`).then(async response => {
+  //         if(response.ok){
+  //             setList(await response.json())
+  //         }
+  //     })
+
+  // },[param])
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,29 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown): boolean =>
+  value === 0 ? false : !value;
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
     // eslint-disable-next-line
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debounceValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {

1456991bf6713206a668516f91878f663bc5da77
lixiaoyi <742805538@qq.com>
2023-09-18 14:49:37 +0800

    chore: 安装devtool

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement,
+// );
+// loadDevTools(() => root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// ))
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root"),
+  ),
 );
-root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
